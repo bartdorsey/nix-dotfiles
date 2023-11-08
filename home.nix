639237a1732585bcb3f0a31f4ecee9b59372bc05
Bart Dorsey <bart@bartdorsey.com>
2023-11-08 13:35:58 -0600

    Initial commit

---
+++ home.nix
@@ -0,0 +1,102 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "echo";
+  home.homeDirectory = "/home/echo";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/echo/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+  # ZSH
+  programs = {
+  	zsh = {
+	  enable = true;
+	  dotDir = ".config/zsh";
+	  };
+         starship = {
+	 	enable = true;
+  		enableZshIntegration = true;
+	};
+	lsd = {
+		enable = true;
+		enableAliases = true;
+	};
+        neovim = {
+		enable = true;
+                vimAlias = true;
+                extraPackages = with pkgs; [
+                  tree-sitter
+                  gcc
+		  nil
+                ];
+		plugins = with pkgs.vimPlugins; [
+		  rose-pine
+                  nvim-treesitter.withAllGrammars
+		  nvim-lspconfig
+                  which-key-nvim
+		];
+		extraLuaConfig = /* lua */''
+		  vim.cmd('colorscheme rose-pine')
+		  local lsp = require'lspconfig'
+		  lsp.nil_ls.setup{}
+		'';
+	};
+        home-manager = {
+		enable = true; 
+	};
+    };
+}
+
